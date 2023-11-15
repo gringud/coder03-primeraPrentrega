4eda0650385f011a0785ec390f36b438f08542fd
nicolasSuarez <ngs_87@hotmail.com>
2023-11-15 12:00:20 -0300

    PreEntrega1Suarez

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,37 @@
+import logoImg from "../../Components/Img/logo.png"
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { TiShoppingCart } from "react-icons/ti";
+
+export default function NavBar(){
+    console.log("object");
+
+    return(
+        <>
+            <Navbar bg="dark" data-bs-theme="dark" collapseOnSelect expand="lg" className="bg-body-tertiary">
+            <Container>
+                <Navbar.Brand href="#home">
+                <img src={logoImg} width="60px"></img>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link href="#Inicio">Inicio</Nav.Link>
+                    <Nav.Link href="#Nosotros">Nosotros</Nav.Link>
+                    <Nav.Link href="#Contacto">Contacto</Nav.Link>
+                    
+                </Nav>
+                <Nav>
+                    <Nav.Link eventKey={2} href="#cart">
+                    <TiShoppingCart className="fs-1"/>
+                    </Nav.Link>
+                    <Nav.Link href="#deets">{1}</Nav.Link>
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+            </Navbar>
+        </>
+    )
+}
